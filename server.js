222900b71d292a91ea32335b4eaf83eb028f2465
Ronan <galv.ronan@gmail.com>
2021-06-19 19:38:56 -0400

    Heroku debug trial #2

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+// Use path to utilize public files (style.css). `app.use(express.static(path.join(__dirname, 'public')));`
+const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-// import sequelize connection
-const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Express-session, etc. (review, delete comment before pushing to main)
-const session = require('express-session');
+const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -20,34 +21,34 @@ const sess = {
 };
 app.use(session(sess));
 
-// Handlebars (review, delete comment before pushing to main)
+// const routes = require('./controllers');
+// import sequelize connection
+
 // Tell handlebars.js about the helpers file
 const helpers = require('./utils/helpers');
-// continue
-const exphbs = require('express-handlebars');
+
 // pass helpers to existing exphbs.create
 const hbs = exphbs.create({
     helpers
 });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
-// Use path to utilize public files (style.css). `app.use(express.static(path.join(__dirname, 'public')));`
-const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-
-// turn on routes
-app.use(routes);
-
 // for public files
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use statements END
 
-// // sync sequelize models to the database, then turn on the server
+// turn on routes
+// app.use(routes);
+
+
+// sync sequelize models to the database, then turn on the server
 sequelize.sync({
     force: true
 }).then(() => {
