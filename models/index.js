63952ececfc70252c7f0eb871bc620c8446d497c
Ronan <galv.ronan@gmail.com>
2021-06-18 23:14:52 -0400

    MVP Finished

--- models/index.js
+++ models/index.js
@@ -14,14 +14,17 @@ Post.belongsTo(User, {
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
+    onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 Post.hasMany(Comment, {
